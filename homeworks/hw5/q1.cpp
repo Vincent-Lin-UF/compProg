36be39271e3b4f0debdfd0243376d2f43281b47d
Vincent Lin <vincentlin1617@gmail.com>
2026-02-16 13:10:31 -0500

    hw5: Dynamic Programming - q1

---
+++ homeworks/hw5/q1.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+static const ll INF = (1LL << 62);
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rep(i,a,b) for (int i = (a); i <= (b); i++)
+#define forn(i,n) for (int i = 0; i < (n); i++)
+#define rforn(i,n) for (int i = (n)-1; i > -1; i--)
+#define rdvec(a,n) do { forn(i,n) cin >> a[i]; } while(0)
+#define print(x) (cout<<#x<<"="<<(x)<<"\n")
+
+static inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+static void solve_one() {
+  int n,p,k;
+  cin >> n >> p >> k;
+
+  string s;
+  cin >> s;
+
+  int x,y;
+  cin >> x >> y;
+  
+  vector<int> missing(n,0);
+  rforn(i,n) {
+    int hasZero = (s[i] == '0') ? 1 : 0;
+    if (i+k < n) missing[i] = hasZero + missing[i+k];
+    else missing[i] = hasZero;
+  }
+
+  ll res = INF;
+  rep(i,p-1,n-1) {
+    ll deletion = i - p + 1;
+    ll cost = deletion * y + 1LL * missing[i] * x;
+    res = min(res,cost);
+  }
+
+  cout << res << "\n";
+}
+
+int main() {
+    fast_io();
+
+    // solve_one();
+    
+
+    int T; cin >> T;
+    while (T--) solve_one();
+    
+
+    // int n;
+    // while (cin >> n && n != 0){
+    //  solve_one(n);
+    // }
+
+    return 0;
+}
+
